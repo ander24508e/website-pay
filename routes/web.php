a4f74e8c26938119ad472702820afc3c7c95463d
Ander <aendara@star-software.net>
2026-03-15 01:51:45 -0500

    Uso de Spatie

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('admin/products', ProductController::class);
+    Route::resource('admin/services', ServiceController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
