e5684b13d7a87c8e1e412d52f454088c4b64e973
Ander <aendara@star-software.net>
2026-08-07 00:52:05 -0500

    Agregar comando en entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ php artisan config:clear  || true
 php artisan cache:clear   || true
 php artisan route:clear   || true
 php artisan view:clear    || true
+php artisan migrate --force   || true
 
 npm install    || true
 composer install    || true
